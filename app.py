0dd51d14a3b43b14b784a3cbf063b167cff0e2bd
Kunal Nehete <kunalnehete@gmail.com>
2025-03-15 23:48:46 +0530

    Final submission-Working html

--- app.py
+++ app.py
@@ -27,6 +27,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    prediction=lrmodel.predict(final_input)
+    return render_template('home.html',prediction_text='Predicted House Price is {}'.format(prediction[0]))
+
 if __name__=="__main__":
     app.run(debug=True)
 
